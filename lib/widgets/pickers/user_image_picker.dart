79f33108766fee6a145839b3ba95be81e3a9cec7
aliDodiya121 <105849535+aliDodiya121@users.noreply.github.com>
2022-10-03 10:11:04 +0530

    Update user_image_picker.dart

--- lib/widgets/pickers/user_image_picker.dart
+++ lib/widgets/pickers/user_image_picker.dart
@@ -6,24 +6,63 @@ import 'package:image_picker/image_picker.dart';
 class UserImagePicker extends StatefulWidget {
   final Function(File image) pickedImage;
   const UserImagePicker({Key? key, required this.pickedImage})
-      : super(key: key);
+      : sup
+      _pickedImage = File(pickedImage?.path ?? "");
+    });
+    widget.pickedImage(_pickedImage!);
+  }
 
   @override
-  State<UserImagePicker> createState() => _UserImagePickerState();
-}
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage!) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: Icon(Icons.camera),
+          label: Text("Add 
+          ckedImage = File(pickedImage?.path ?? "");
+    });
+    widget.pickedImage(_pickedImage!);
+  }
 
-class _UserImagePickerState extends State<UserImagePicker> {
-  File? _pickedImage;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage!) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: Icon(Icons.camera),
+          label: Text("Add 
+                      ckedImage = File(pickedImage?.path ?? "");
+    });
+    widget.pickedImage(_pickedImage!);
+  }
 
-  void _pickImage() async {
-    final pickedImage = await ImagePicker().pickImage(
-      source: ImageSource.camera,
-      imageQuality: 50,
-      maxWidth: 200,
-    );
-
-    setState(() {
-      _pickedImage = File(pickedImage?.path ?? "");
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage!) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: Icon(Icons.camera),
+          label: Text("Add 
+          
+          ckedImage = File(pickedImage?.path ?? "");
     });
     widget.pickedImage(_pickedImage!);
   }
